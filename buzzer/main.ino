d47b21bda55042a44ac1431401210f8ec4febc7f
Govind raj <govind.mndy@gmail.com>
2020-01-27 21:28:55 -0500

    Update main.ino

--- buzzer/main.ino
+++ buzzer/main.ino
@@ -12,3 +12,19 @@ void loop()
   delay(800); // Wait for 1000 millisecond(s)
 }
 
+//using tone library
+
+int buzzer=1;
+void setup()
+{
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop()
+{
+  tone(1,500);
+  delay(100);
+  noTone(1);
+  delay(100);
+}
+
