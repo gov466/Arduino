9a82ed9f9b575dd9ee6a39da3068eb880e78dd9b
Govind raj <govind.mndy@gmail.com>
2020-01-28 20:23:30 -0500

    Update main.ino

--- pushbutton_led/main.ino
+++ pushbutton_led/main.ino
@@ -18,3 +18,36 @@ void loop()
   digitalWrite(led, LOW);
   }
 }
+
+//two swtch operated on two different leds
+int led1=1;
+int sw1=2;
+int led2=4;
+int sw2=3;
+void setup()
+{
+  pinMode(led1, OUTPUT);
+  pinMode(sw1,INPUT_PULLUP);
+   pinMode(led2, OUTPUT);
+  pinMode(sw2,INPUT_PULLUP);
+}
+
+void loop()
+{
+  if(digitalRead(sw1) == LOW)
+  {
+  digitalWrite(led1, HIGH);
+  }
+ if(digitalRead(sw1) == HIGH)
+  {
+  digitalWrite(led1, LOW);
+  }
+  if(digitalRead(sw2) == LOW)
+  {
+  digitalWrite(led2, HIGH);
+  }
+ if(digitalRead(sw2) == HIGH)
+  {
+  digitalWrite(led2, LOW);
+  }
+}
