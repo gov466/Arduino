a25c8d7105cda40cd738dfddb9e71a0e761f2828
Govind raj <govind.mndy@gmail.com>
2020-01-30 21:46:04 -0500

    completed blinking led while pressing sw1
    should add loop and sw2

--- Blinking_led_with_swanddelay/main.ino
+++ Blinking_led_with_swanddelay/main.ino
@@ -8,19 +8,32 @@ void setup()
   pinMode(sw1,INPUT_PULLUP);
    pinMode(led2, OUTPUT);
   pinMode(sw2,INPUT_PULLUP);
+  //digitalWrite(led1, LOW);
+}
+void blink()
+{
+  if(digitalRead(sw1) == LOW)
+  {
+    delay(500);
+  while(digitalRead(sw1)==HIGH)
+  {
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  delay(500);
+  }
+  }
 }
 
 void loop()
 {
-  if(digitalRead(sw1) == HIGH)
+  for(int i=0;i<5;i++)
   {
-  digitalWrite(led1, HIGH);
-  //digitalWrite(led2, HIGH);
-   delay(500);
-     digitalWrite(led1, LOW);
-    delay(500);
+    blink();
   }
- if(digitalRead(sw2) == HIGH)
+/* if(digitalRead(sw2) == HIGH)
   {
    digitalWrite(led2, HIGH);
    delay(500);
