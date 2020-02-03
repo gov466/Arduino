66ab2b7e6c4a2b913ed92f512f00c092fcb16856
Govind raj <govind.mndy@gmail.com>
2020-02-02 22:20:51 -0500

    Update main.ino

--- LCD Display/namedisplay/main.ino
+++ LCD Display/namedisplay/main.ino
@@ -1,3 +1,4 @@
+//displaying govind raj in lcd and running a counter from which lcd is on
 #include <LiquidCrystal.h>
 const int rs=2,en=3,d4=4,d5=5,d6=6,d7=7;
 LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
