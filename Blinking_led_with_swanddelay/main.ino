856516c36901f94430d23584816314674bbdbc5b
Govind raj <govind.mndy@gmail.com>
2020-02-04 22:11:28 -0500

    Update main.ino

--- Blinking_led_with_swanddelay/main.ino
+++ Blinking_led_with_swanddelay/main.ino
@@ -1,44 +1,72 @@
 //blink led with delay and loop
-int led1=1; 
-int sw1=2;
-int led2=4;
-int sw2=3;
+int o=2;
+int p=3;
+int e=4;
+int n=5;
+int sw1=13;
 void setup()
 {
-  pinMode(led1, OUTPUT);
+  pinMode(o, OUTPUT);
+  pinMode(p, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(n, OUTPUT);
   pinMode(sw1,INPUT_PULLUP);
-   pinMode(led2, OUTPUT);
-  pinMode(sw2,INPUT_PULLUP);
 }
-
+void open()
+{
+  digitalWrite(o, HIGH);
+  //delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(p, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(n, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(o, LOW);
+  digitalWrite(p,HIGH);
+  digitalWrite(e, LOW);
+  digitalWrite(n, LOW);
+  delay(500); 
+  digitalWrite(o, LOW);
+  digitalWrite(p, LOW);
+  digitalWrite(e, HIGH);
+  digitalWrite(n, LOW);
+  delay(500);
+  digitalWrite(o, LOW);
+  digitalWrite(p,LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(n, HIGH);
+  delay(500);
+}
 
 void loop()
 {
-  if(digitalRead(sw1) == LOW)
-  {
-  for(int i=0;i<5;i++)
-  {
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, HIGH);
-  delay(250);
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
-  delay(250);
-  }
   
-  }
-if(digitalRead(sw2) == LOW)
+ // if(digitalRead(sw1) == LOW)
+//{
+  for(int i=0;i<2;i++)
   {
-  for(int i=0;i<10;i++)
-  {
-   
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, HIGH);
-  delay(1000);
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
-  delay(1000);
+  open();
   }
- 
+  while(1)
+  {
+  digitalWrite(o, HIGH);
+  digitalWrite(p, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(n, HIGH);
+  delay(500);
+  digitalWrite(o, LOW);
+  digitalWrite(p, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(n, LOW);
+  delay(500);
+     
+    
   }
 }
+ /* if( int i==2)
+  {
+  
+  }
+ 
+//}*/
+
+    
