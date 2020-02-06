251fb2ed2e1e58197dd30272746a10c15695e952
Govind raj <govind.mndy@gmail.com>
2020-02-05 21:51:14 -0500

    Update main.ino

--- Open_board_led_design/main.ino
+++ Open_board_led_design/main.ino
@@ -1,33 +1,107 @@
+int o=2;
+int p=3;
+int e=4;
+int n=5;
+int sw1=13;
+int led1=6;
+int led2=7;
+int led3=8;
+int led4=9;
+int led5=10;
 void setup()
 {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+  pinMode(o, OUTPUT);
+  pinMode(p, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(n, OUTPUT);
+  pinMode(sw1,INPUT_PULLUP);
+}
+void open()
+{
+  digitalWrite(o, HIGH);
+  //delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(p, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(n, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(o, LOW);
+  digitalWrite(p,HIGH);
+  digitalWrite(e, LOW);
+  digitalWrite(n, LOW);
+  delay(500); 
+  digitalWrite(o, LOW);
+  digitalWrite(p, LOW);
+  digitalWrite(e, HIGH);
+  digitalWrite(n, LOW);
+  delay(500);
+  digitalWrite(o, LOW);
+  digitalWrite(p,LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(n, HIGH);
+  delay(500);
 }
 
 void loop()
 {
-  digitalWrite(2, HIGH);
-  //delay(500); // Wait for 1000 millisecond(s)
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  delay(500); // Wait for 1000 millisecond(s)
-  digitalWrite(2, LOW);
-  digitalWrite(3,HIGH);
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  delay(500); 
-  digitalWrite(2, LOW);
-  digitalWrite(3,LOW);
-  digitalWrite(4, HIGH);
-  digitalWrite(5, LOW);
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  delay(250);
+  digitalWrite(led1,LOW);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  delay(250);
+  digitalWrite(led1,LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  delay(250);
+  digitalWrite(led1,LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, LOW);
+  delay(250);
+  digitalWrite(led1,LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, HIGH);
+  delay(250);
+  
+  
+ // if(digitalRead(sw1) == LOW)
+//{
+  for(int i=0;i<2;i++)
+  {
+  open();
+  }
+  while(1)
+  {
+  digitalWrite(o, HIGH);
+  digitalWrite(p, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(n, HIGH);
   delay(500);
-  digitalWrite(2, LOW);
-  digitalWrite(3,LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(5, HIGH);
+  digitalWrite(o, LOW);
+  digitalWrite(p, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(n, LOW);
   delay(500);
+     
+    
+  }
 }
+ /* if( int i==2)
+  {
+  
+  }
+ 
+//}*/
+
+    
