64f968fa12e093ea2dde9cfa6cc8854e8d31fe03
Govind raj <govind.mndy@gmail.com>
2020-02-19 21:29:08 -0500

    completed

--- Open_board_led_design/main.ino
+++ Open_board_led_design/main.ino
@@ -1,10 +1,11 @@
+//OPEN board design 
 int o=2;
 int p=3;
 int e=4;
 int n=5;
 int l=6;
 int sw1=13;
-void setup()
+void setup() //setup
 {
   pinMode(o, OUTPUT);
   pinMode(p, OUTPUT);
@@ -14,75 +15,64 @@ void setup()
   
     
 }
-void open()
+void open() //function for blinking each alphabet
 {
   digitalWrite(o, HIGH);
   //delay(500); // Wait for 1000 millisecond(s)
   digitalWrite(p, LOW);
   digitalWrite(e, LOW);
   digitalWrite(n, LOW);
-  delay(500); // Wait for 1000 millisecond(s)
+  delay(250); // Wait for 1000 millisecond(s)
   digitalWrite(o, LOW);
   digitalWrite(p,HIGH);
   digitalWrite(e, LOW);
   digitalWrite(n, LOW);
-  delay(500); 
+  delay(250); 
   digitalWrite(o, LOW);
   digitalWrite(p, LOW);
   digitalWrite(e, HIGH);
   digitalWrite(n, LOW);
-  delay(500);
+  delay(250);
   digitalWrite(o, LOW);
   digitalWrite(p,LOW);
   digitalWrite(e, LOW);
   digitalWrite(n, HIGH);
-  delay(500);
+  delay(250);
 }
-void led()
+void led()    //funtion for blinking circular led
 {
   digitalWrite(l, HIGH);
-    delay(550);
+    delay(250);
     digitalWrite(l, LOW);
-    delay(550);
+    delay(250);
 }
-void loop()
+void loop() //loop for whole function
 {
-  /*while(1)
-  {
-    digitalWrite(l, HIGH);
-    delay(250);
-    digitalWrite(l, LOW);
-    delay(250);
-  }*/
- // if(digitalRead(sw1) == LOW)
-//{
- int j=2;
-  if(j==2)
-  {
-    led();
-  }
-  for(int i=0;i<2;i++)
-  {
-  open();
   //led();
-  }
-  for(int i=0;i<2;i++)
+  open();
+  for(int i=0;i<2;i++)  //loop for blinking the whole OPEN 2 times
   {
   
   digitalWrite(o, HIGH);
   digitalWrite(p, HIGH);
   digitalWrite(e, HIGH);
   digitalWrite(n, HIGH);
-  delay(500);
+  delay(250);
   digitalWrite(o, LOW);
   digitalWrite(p, LOW);
   digitalWrite(e, LOW);
   digitalWrite(n, LOW);
-  delay(500);
+  delay(250);
      
     
-  }
-  delay(1500);
+  } // end of loop  
+  digitalWrite(o, HIGH);  //making OPEN all high
+  digitalWrite(p, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(n, HIGH);
+  led();
+  
+  delay(1500);  //calling a delay of 1500 after finishing whole loop once
 }
 
  
