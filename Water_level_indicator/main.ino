5d862c0157e6403dfa6f3b68d12ce169a4f2e0f4
Govind raj <govind.mndy@gmail.com>
2020-04-20 21:39:58 -0400

    Update main.ino

--- Water_level_indicator/main.ino
+++ Water_level_indicator/main.ino
@@ -1,32 +1,63 @@
+/*
+  LiquidCrystal Library - scrollDisplayLeft() and scrollDisplayRight()
 
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This sketch prints "Hello World!" to the LCD and uses the
+ scrollDisplayLeft() and scrollDisplayRight() methods to scroll
+ the text.
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+*/
+
+// include the library code:
 #include <LiquidCrystal.h>
-const int rs=8,en=9,d4=4,d5=5,d6=6,d7=7;
-LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 const int echoPin = 2; // Echo Pin of Ultrasonic Sensor
 const int pingPin = 3; // Trigger Pin of Ultrasonic Sensor
-const int ledred = 10; //pin for red led
-const int ledgreen = 11; //pin for green led
-const int buzzer = 1; //pin for buzzer
-
-
-                    
-void setup() 
-{
+void setup() {
+  
   Serial.begin(9600); // Starting Serial Communication
   pinMode(pingPin, OUTPUT); // initialising pin 3 as output
   pinMode(echoPin, INPUT); // initialising pin 2 as input
-  pinMode(ledred,OUTPUT); //initialising pin 10 as output
-  pinMode(ledgreen,OUTPUT); //initialising pin 11 as output
-  pinMode(buzzer,OUTPUT);  //initialising pin 1 as output
-  lcd.begin(16,2);
-  lcd.setCursor(0,0);
+  
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  for(int i=0;i<3;i++)
+  {
+  lcd.setCursor(3, 0); 
+  lcd.print("Water Level ");
+  lcd.setCursor(4, 1);
+  lcd.print("Indicator");
+  delay(1500);
+  lcd.clear();
+  delay(500);
+  }
+  
+  // Print a message to the LCD.
+   
   lcd.print("Water level indicator");
+  delay(1000);
 }
 
-void loop()
-{
-  long duration, inches, cm;
+void loop() {
+   long duration, inches, cm;
   
   digitalWrite(pingPin, LOW);
   delayMicroseconds(2);
@@ -40,77 +71,71 @@ void loop()
   duration = pulseIn(echoPin, HIGH); // using pulsin function to determine total time
   inches = microsecondsToInches(duration); // calling method
   cm = microsecondsToCentimeters(duration); // calling method
-
-
+  if((cm<20)&&(cm>11))
+  {
+  lcd.setCursor(0,0);
+  lcd.print("Water level indicator");
+  }
   lcd.setCursor(0,1);
   lcd.print(inches);
   lcd.print("in. ");
   lcd.print(cm);
   lcd.print("cm");
-
-  if(cm>500)
+ 
+  if(cm>20)
   {
+    lcd.clear();
     lcd.print("Overflow");
-    digitalWrite(ledgreen, HIGH);
-    digitalWrite(ledred, LOW);
+    
+    //digitalWrite(ledgreen, HIGH);
+    //digitalWrite(ledred, LOW);
     //digitalWrite(buzzer, HIGH);
     
     
   }
-  else if(cm<400)
+  else if(cm<11)
   {
+    lcd.clear();
     lcd.print("underflow");
-    digitalWrite(ledred, HIGH);
-    digitalWrite(ledgreen, LOW);
+    //digitalWrite(ledred, HIGH);
+    //digitalWrite(ledgreen, LOW);
     //digitalWrite(buzzer, LOW);
 
   }
-  if(cm>500)
-  {
-    digitalWrite(buzzer, HIGH);
-  }
-  else
-  {
-    digitalWrite(buzzer, LOW);
-  }
   
-   // scroll 13 positions (string length) to the left
+  
+  
+  // scroll 13 positions (string length) to the left
   // to move it offscreen left:
-/*  for (int positionCounter = 0; positionCounter < 21; positionCounter++) {
+ /* for (int positionCounter = 0; positionCounter < 13; positionCounter++) {
     // scroll one position left:
     lcd.scrollDisplayLeft();
     // wait a bit:
-    delay(150);
+    delay(500);
   }
 
   // scroll 29 positions (string length + display length) to the right
   // to move it offscreen right:
-  for (int positionCounter = 0; positionCounter < 37; positionCounter++) {
+  for (int positionCounter = 0; positionCounter < 29; positionCounter++) {
     // scroll one position right:
     lcd.scrollDisplayRight();
     // wait a bit:
-    delay(150);
+    delay(500);
   }
 
   // scroll 16 positions (display length + string length) to the left
   // to move it back to center:
-  for (int positionCounter = 0; positionCounter < 37; positionCounter++) {
+  for (int positionCounter = 0; positionCounter < 16; positionCounter++) {
     // scroll one position left:
     lcd.scrollDisplayLeft();
     // wait a bit:
-    delay(150);
-  } */
-  
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
-  
-  delay(100);
+    delay(500);
+  }
+
+  // delay at the end of the full loop:
+  delay(1000);*/
 
 }
-
 long microsecondsToInches(long microseconds) // method to covert microsec to inches 
 {
  return microseconds / 74 / 2;
