9c5b68357889803f65e4c0012e13e00bd6186cfe
gov466 <govind.mndy@gmail.com>
2020-08-25 18:10:29 -0400

    uart added

--- Uart/Uart.c/Uart.c.ino
+++ Uart/Uart.c/Uart.c.ino
@@ -1,76 +1,53 @@
-int ledpin = 8;
+int ledpin = 10;
 void setup() {
-   // Choose a baud rate and configuration. 115200
-   // Default is 8-bit with No parity and 1 stop bit
-   Serial.begin(9600, SERIAL_8N1);
-   pinMode(ledpin , OUTPUT);
-   pinMode(LED_BUILTIN , OUTPUT);
-   
-   
-}
+  // Choose a baud rate and configuration. 115200
+  // Default is 8-bit with No parity and 1 stop bit
+  Serial.begin(9600, SERIAL_8N1);
+  pinMode(ledpin , OUTPUT);
+  pinMode(LED_BUILTIN , OUTPUT);
 
+
+}
+char charIn;
 // This function will loop as quickly as possible, forever.
 void loop() {
-   byte charIn;
-   if(Serial.available()){          // A byte has been received
-      charIn = Serial.read();       // Read the character in from the BBB
-      //Serial.println(charIn);
-      Serial.write(charIn);         // Send the character back to the BBB
-      if(charIn == 'M')
-       {
-        while(1)
-        {
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(250);
-        Serial.println("Led is blinking with delay 250");
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(250);
-                  // A byte has been received
-          charIn = Serial.read();
-          
-        
-          
-        }
-         
-       }
-       if(charIn == 'L')
-       {
-        while(1)
-        {
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(500);
-        Serial.println("Led is blinking with delay 500");
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(500);
-        }
-       }
-       if(charIn == 'H')
-       {
-        for(;;)
-        { 
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(1000);
-        Serial.println("Led is blinking with delay 1000");
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(1000);
-        }
-       
-       }
-       if (charIn == 'A')
-       {
-        for(;;)
-        {
-          digitalWrite(ledpin, HIGH);
-          delay(1000);
-          digitalWrite(ledpin, LOW);
-          delay(1000);
-        }
-        
-       }
 
-      
-   }
-  
- 
+  if (Serial.available() > 0) {      // A byte has been received
+    charIn = Serial.read();       // Read the character in from the BBB
+    //Serial.println(charIn);
+    //Serial.println("led flashing at frequency");Serial.println( charIn);
+    Serial.write(charIn);
+  }// Send the character back to the BBB
+  if (charIn == 'M')
+  {
+    analogWrite(ledpin, 140);
+    analogWrite(LED_BUILTIN, 140);
+    Serial.println("led flashing at frequency 140");
+    
+
+  }
+  if (charIn == 'L')
+  {
+    analogWrite(ledpin, 200);
+    analogWrite(LED_BUILTIN, 200);
+    Serial.println("led flashing at frequency 200");
    
-} 
+  }
+  if (charIn == 'H')
+  {
+    analogWrite(ledpin, 255);
+    analogWrite(LED_BUILTIN, 255);
+    Serial.println("led flashing at frequency 255");
+    
+  }
+  if (charIn == 'A')
+  {
+    analogWrite(ledpin, 0);
+    analogWrite(LED_BUILTIN, 0);
+    Serial.println("led flashing at frequency 0 -LED OFF");
+
+
+  }
+
+
+}
--- uart_shedule_loop/uart.ino/uart.ino.ino
+++ uart_shedule_loop/uart.ino/uart.ino.ino
@@ -1,91 +1,70 @@
-#include<Scheduler.h>
-int ledpin = 8;
+int ledpin = 10;
 void setup() {
-   // Choose a baud rate and configuration. 115200
-   // Default is 8-bit with No parity and 1 stop bit
-   Serial.begin(9600, SERIAL_8N1);
-   pinMode(ledpin , OUTPUT);
-   pinMode(LED_BUILTIN , OUTPUT);
+  // Choose a baud rate and configuration. 115200
+  // Default is 8-bit with No parity and 1 stop bit
+  Serial.begin(9600, SERIAL_8N1);
+  pinMode(ledpin , OUTPUT);
+  pinMode(LED_BUILTIN , OUTPUT);
 
-   Scheduler.startLoop(loop2);
-   Scheduler.startLoop(loop3);
-   
-   
+
 }
-
+int charIn;
 // This function will loop as quickly as possible, forever.
 void loop() {
-   byte charIn;
-   if(Serial.available()){          // A byte has been received
-      charIn = Serial.read();       // Read the character in from the BBB
-      //Serial.println(charIn);
-      Serial.write(charIn);         // Send the character back to the BBB
-      
-   }
-   if(charIn == 'M')
-   {
-    while(1)
-    {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(250);
-    Serial.println("Led is blinking");
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(250);
-    }
-   }
-}
-void loop2()
-{
-  byte charIn;
-   if(Serial.available()){          // A byte has been received
-      charIn = Serial.read();       // Read the character in from the BBB
-      //Serial.println(charIn);
-      Serial.write(charIn);         // Send the character back to the BBB
-      
-   }
-  
-   if(charIn == 'L')
-   {
-    while(1)
-    {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
-    }
-   }
-}
-void loop3()
-{
-   byte charIn;
-   if(Serial.available()){          // A byte has been received
-      charIn = Serial.read();       // Read the character in from the BBB
-      //Serial.println(charIn);
-      Serial.write(charIn);         // Send the character back to the BBB
-      
-   }
-   if(charIn == 'H')
-   {
-    for(;;)
-    { 
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
-    }
-   
-   }
-   if (charIn == 'A')
-   {
-    for(;;)
-    {
-      digitalWrite(ledpin, HIGH);
+
+  if (Serial.available()) {      // A byte has been received
+    charIn = Serial.read();       // Read the character in from the BBB
+    Serial.println("led flashing at %c frequency",charIn);
+    Serial.write(charIn);
+  }// Send the character back to the BBB
+  //if (charIn == 'M')
+  //{
+    analogWrite(ledpin, charIn);
+    /* digitalWrite(LED_BUILTIN, HIGH);
+      delay(250);
+      Serial.println("Led is blinking with delay 250");
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(250);
+      // A byte has been received
+    */
+
+  //}
+/*  if (charIn == 'L')
+  {
+    analogWrite(ledpin, 150);
+    /*  digitalWrite(LED_BUILTIN, HIGH);
+      delay(500);
+      Serial.println("Led is blinking with delay 500");
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(500);
+    
+  }
+  if (charIn == 'H')
+  {
+    analogWrite(ledpin, 250);
+    /*   digitalWrite(LED_BUILTIN, HIGH);
+       delay(1000);
+       Serial.println("Led is blinking with delay 1000");
+       digitalWrite(LED_BUILTIN, LOW);
+       delay(1000);
+
+    
+  }
+  if (charIn == 'A')
+  {
+    analogWrite(ledpin, 0);
+
+    /*  digitalWrite(ledpin, HIGH);
       delay(1000);
       digitalWrite(ledpin, LOW);
       delay(1000);
-    }
     
-   }
- yield();
-   
-} 
+
+  } */
+
+
+
+
+
+
+}
