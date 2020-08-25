bae0d2340db07b0c87e5e638bdf102f3cbb1d7eb
gov466 <govind.mndy@gmail.com>
2020-08-24 23:39:40 -0400

    uart added

--- Uart/Uart.c/Uart.c.ino
+++ Uart/Uart.c/Uart.c.ino
@@ -1,21 +1,76 @@
-//int ledpin = 8;
+int ledpin = 8;
 void setup() {
    // Choose a baud rate and configuration. 115200
    // Default is 8-bit with No parity and 1 stop bit
-   Serial.begin(115200, SERIAL_8N1);
-  // pinMode(ledpin , OUTPUT);
+   Serial.begin(9600, SERIAL_8N1);
+   pinMode(ledpin , OUTPUT);
+   pinMode(LED_BUILTIN , OUTPUT);
+   
    
 }
 
 // This function will loop as quickly as possible, forever.
 void loop() {
    byte charIn;
-   if(S;erial.available()){          // A byte has been received
+   if(Serial.available()){          // A byte has been received
       charIn = Serial.read();       // Read the character in from the BBB
       //Serial.println(charIn);
       Serial.write(charIn);         // Send the character back to the BBB
+      if(charIn == 'M')
+       {
+        while(1)
+        {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(250);
+        Serial.println("Led is blinking with delay 250");
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(250);
+                  // A byte has been received
+          charIn = Serial.read();
+          
+        
+          
+        }
+         
+       }
+       if(charIn == 'L')
+       {
+        while(1)
+        {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(500);
+        Serial.println("Led is blinking with delay 500");
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(500);
+        }
+       }
+       if(charIn == 'H')
+       {
+        for(;;)
+        { 
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(1000);
+        Serial.println("Led is blinking with delay 1000");
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(1000);
+        }
+       
+       }
+       if (charIn == 'A')
+       {
+        for(;;)
+        {
+          digitalWrite(ledpin, HIGH);
+          delay(1000);
+          digitalWrite(ledpin, LOW);
+          delay(1000);
+        }
+        
+       }
+
       
    }
+  
  
    
 } 
