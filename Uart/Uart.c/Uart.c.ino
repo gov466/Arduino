036ea5608b2e17c9a77f6f1efb18aed4465fb6c7
gov466 <govind.mndy@gmail.com>
2020-10-13 23:05:35 -0400

    esp8266 tested

--- Uart/Uart.c/Uart.c.ino
+++ Uart/Uart.c/Uart.c.ino
@@ -14,36 +14,34 @@ void loop() {
 
   if (Serial.available() > 0) {      // A byte has been received
     charIn = Serial.read();       // Read the character in from the BBB
-    //Serial.println(charIn);
-    //Serial.println("led flashing at frequency");Serial.println( charIn);
-    Serial.write(charIn);
-  }// Send the character back to the BBB
-  if (charIn == 'M')
+    
+  }
+  if (charIn == 'M') //if chaaracter recieved is M the get into this function
   {
     analogWrite(ledpin, 140);
-    analogWrite(LED_BUILTIN, 140);
+    analogWrite(LED_BUILTIN, 140); //led at 140 brightness
     Serial.println("led flashing at frequency 140");
     
 
   }
-  if (charIn == 'L')
+  if (charIn == 'L') //if chaaracter recieved is L the get into this function
   {
     analogWrite(ledpin, 200);
-    analogWrite(LED_BUILTIN, 200);
+    analogWrite(LED_BUILTIN, 200); //led at 200 brightness
     Serial.println("led flashing at frequency 200");
    
   }
-  if (charIn == 'H')
+  if (charIn == 'H')//if chaaracter recieved is H the get into this function
   {
     analogWrite(ledpin, 255);
-    analogWrite(LED_BUILTIN, 255);
+    analogWrite(LED_BUILTIN, 255); //led at 255 brightness
     Serial.println("led flashing at frequency 255");
     
   }
-  if (charIn == 'A')
+  if (charIn == 'A')////if chaaracter recieved is A the get into this function
   {
     analogWrite(ledpin, 0);
-    analogWrite(LED_BUILTIN, 0);
+    analogWrite(LED_BUILTIN, 0); //turn off led
     Serial.println("led flashing at frequency 0 -LED OFF");
 
 
